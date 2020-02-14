ce8075c97c449c98a5e0d030a869349b687d1a66
Gaurav Raj <graj2@illinois.edu>
2020-02-14 00:26:09 -0600

    Finished MP3

--- mp3/mp3.c
+++ mp3/mp3.c
@@ -9,6 +9,17 @@ int main()
   scanf("%d",&row);
 
   // Write your code here
+  unsigned long i,j,out;
 
+  for (i=0; i<= row; ++i)
+  {
+      out = 1;
+      for (j=1; j<=i; ++j)
+      {
+          out = (out * (row + 1 - j)) / j;
+      }
+      printf("%lu", out);
+      printf(" ");
+  }
   return 0;
 }
