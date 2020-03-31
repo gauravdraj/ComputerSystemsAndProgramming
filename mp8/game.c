7e314c22d0d43bda619de71b50feac1ce2618ba5
Gaurav Raj <graj2@illinois.edu>
2020-03-31 01:46:19 -0500

    MP8 Finalized

--- mp8/game.c
+++ mp8/game.c
@@ -1,5 +1,27 @@
 #include "game.h"
 
+/*
+This program simulates 2048, a game that I play a lot on long plane rides.
+make_game initializes a game and sets the rows and columns according to passed
+in parameters, and the score is set to 0. All elements of the board are set to
+-1 (empty) and a pointer to the game is returned. remake_game works similarly,
+it takes the current board pointer and sets the rows and columns, according to
+passed in parameters and sets the score to 0, but doesn't return anything.
+All the move functions (move_w, move_a, move_s, and move_d) work similarly.
+They iterate through the board by rows/cols or cols/rows depending on the
+function, and finds the first available empty spot for each element. They then
+swap the values of the cells and sets a change flag to true. It then combines
+with the cell adjacent to it if that cell hasn't been combined. The original
+cell is set to empty and is ready for the next iteration. If the change flag
+has been set, then they return 1. If not, they return 0. get_cell works by
+checking if the provided row and column are in range, and then returning a
+pointer to the element there. If not, it returns NULL. legal_move_check works
+by iterating through the board and returning 1 if there are any empty cells
+or if any cell adjacent to the current one has the same value as the current
+one. If none of these conditions are met, legal_move_check returns 0. 
+
+partner: pranay2
+*/
 
 game * make_game(int rows, int cols)
 /*! Create an instance of a game structure with the given number of rows
@@ -14,7 +36,15 @@ game * make_game(int rows, int cols)
     mygame->cells = malloc(rows*cols*sizeof(cell));
 
     //YOUR CODE STARTS HERE:  Initialize all other variables in game struct
-
+    (*mygame).rows = rows;
+    (*mygame).cols = cols;
+    (*mygame).score = 0;
+    int i, j;
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            (*mygame).cells[(i * cols) + j] = -1;
+        }
+    }
 
     return mygame;
 }
@@ -32,7 +62,15 @@ void remake_game(game ** _cur_game_ptr,int new_rows,int new_cols)
 	(*_cur_game_ptr)->cells = malloc(new_rows*new_cols*sizeof(cell));
 
 	 //YOUR CODE STARTS HERE:  Re-initialize all other variables in game struct
-
+    (**_cur_game_ptr).rows = new_rows;
+    (**_cur_game_ptr).cols = new_cols;
+    (**_cur_game_ptr).score = 0;
+    int i, j;
+    for (i = 0; i < new_rows; i++) {
+        for (j = 0; j < new_cols; j++) {
+            (**_cur_game_ptr).cells[(i * new_cols) + j] = -1;
+        }
+    }
 	return;	
 }
 
@@ -55,7 +93,11 @@ cell * get_cell(game * cur_game, int row, int col)
 {
     //YOUR CODE STARTS HERE
 
-    return NULL;
+    if ((0 <= row && row <= (*cur_game).rows) && (0 <= col && col <= (*cur_game).cols)) { //if in range
+        return (*cur_game).cells + (row * (*cur_game).cols + col); //return pointer to cell
+    }
+
+    return NULL; //if not in range, return NULL
 }
 
 int move_w(game * cur_game)
@@ -67,29 +109,177 @@ int move_w(game * cur_game)
 */
 {
     //YOUR CODE STARTS HERE
+    int i, j; //iterators - col and row
+    bool boardFlag = false; //boardFlag detects if something has been moved or combined
+    int targetRow = -1; //first available empty location before current row
+    int lastCombined = -1; //last combined row
+    bool emptyFound = false; //true when there's an empty cell before current row
+    int numCols = (*cur_game).cols; //i don't want to type this out every time
 
+    for (i = 0; i < (*cur_game).cols; i++) {
+        lastCombined = -1; //resest lastCombined each column
+        for (j = 0; j < (*cur_game).rows; j++) {
+            int *valAtij = &(*cur_game).cells[(j * numCols) + i];
+            if (*valAtij == -1) continue; //skip iteration if row j at col i is empty
+            emptyFound = false; //reset emptyFound
+            for (targetRow = 0; targetRow < j; targetRow++) {
+                if ((*cur_game).cells[(targetRow * numCols) + i] == -1) {
+                    emptyFound = true; //if empty location found before current row, set emptyFound flag and break
+                    break;
+                }
+            }
+            int *targetVal = &(*cur_game).cells[(targetRow * numCols) + i]; //pointer because i'm lazy
+            if (emptyFound) { 
+                *targetVal = *valAtij; //switch values if there's an empty spot to switch with
+                *valAtij = -1;
+                boardFlag = true; //indicate something changed
+            }
+            int *beforeTargetVal = &(*cur_game).cells[((targetRow - 1) * numCols) + i]; //pointer 2 because i'm lazy
+            if ((targetRow - 1) != lastCombined && *beforeTargetVal == *targetVal) { //if previous row hasn't been combined and if same val
+                *beforeTargetVal *= 2; //set the first block to double its value
+                (*cur_game).score += *beforeTargetVal; //update score
+                *targetVal = -1; //make second block empty
+                lastCombined = targetRow - 1; //update lastCombined
+                boardFlag = true; //indicate something changed
+            }
+        }
+    }
+    if (!boardFlag) return 0; //return 0 if nothing changed, if not return 1
     return 1;
-};
+}
 
 int move_s(game * cur_game) //slide down
 {
     //YOUR CODE STARTS HERE
+    int i, j; //iterators - col and row
+    bool boardFlag = false; //boardFlag detects if something has been moved or combined
+    int targetRow = -1; //first available empty location before current row
+    int lastCombined = -1; //last combined row
+    bool emptyFound = false; //true when there's an empty cell before current row
+    int numCols = (*cur_game).cols; //i don't want to type this out every time
 
+    for (i = 0; i < (*cur_game).cols; i++) {
+        lastCombined = -1; //resest lastCombined each column
+        for (j = (*cur_game).rows - 1; j > -1; j--) {
+            int *valAtij = &(*cur_game).cells[(j * numCols) + i];
+            if (*valAtij == -1) continue; //skip iteration if row j at col i is empty
+            emptyFound = false; //reset emptyFound
+            for (targetRow = (*cur_game).rows - 1; targetRow > j; targetRow--) {
+                if ((*cur_game).cells[(targetRow * numCols) + i] == -1) {
+                    emptyFound = true; //if empty location found after current row, set emptyFound flag and break
+                    break;
+                }
+            }
+            int *targetVal = &(*cur_game).cells[(targetRow * numCols) + i]; //pointer because i'm lazy
+            if (emptyFound) { 
+                *targetVal = *valAtij; //switch values if there's an empty spot to switch with
+                *valAtij = -1;
+                boardFlag = true; //indicate something changed
+            }
+            int *afterTargetVal = &(*cur_game).cells[((targetRow + 1) * numCols) + i]; //pointer 2 because i'm lazy
+            if ((targetRow + 1) != lastCombined && *afterTargetVal == *targetVal) { //if previous row hasn't been combined and if same val
+                *afterTargetVal *= 2; //set the first block to double its value
+                (*cur_game).score += *afterTargetVal; //update score
+                *targetVal = -1; //make second block empty
+                lastCombined = targetRow + 1; //update lastCombined
+                boardFlag = true; //indicate something changed
+            }
+        }
+    }
+    if (!boardFlag) return 0; //return 0 if nothing changed, if not return 1
     return 1;
-};
+}
 
 int move_a(game * cur_game) //slide left
 {
     //YOUR CODE STARTS HERE
+    int i, j; //iterators - row and col
+    bool boardFlag = false; //boardFlag detects if something has been moved or combined
+    int targetCol = -1; //first available empty location before current col
+    int lastCombined = -1; //last combined col
+    bool emptyFound = false; //true when there's an empty cell before current col
+    int numCols = (*cur_game).cols; //i don't want to type this out every time
 
+    for (i = 0; i < (*cur_game).rows; i++) {
+        lastCombined = -1; //resest lastCombined each row
+        for (j = 0; j < (*cur_game).cols; j++) {
+            int *valAtij = &(*cur_game).cells[(i * numCols) + j];
+            if (*valAtij == -1) continue; //skip iteration if col j at row i is empty
+            emptyFound = false; //reset emptyFound
+            for (targetCol = 0; targetCol < j; targetCol++) {
+                if ((*cur_game).cells[(i * numCols) + targetCol] == -1) {
+                    emptyFound = true; //if empty location found before current col, set emptyFound flag and break
+                    break;
+                }
+            }
+            int *targetVal = &(*cur_game).cells[(i * numCols) + targetCol]; //pointer because i'm lazy
+            if (emptyFound) { 
+                *targetVal = *valAtij; //switch values if there's an empty spot to switch with
+                *valAtij = -1;
+                boardFlag = true; //indicate something changed
+            }
+            int *beforeTargetVal = &(*cur_game).cells[(i * numCols) + targetCol - 1]; //pointer 2 because i'm lazy
+            if ((targetCol - 1) != lastCombined && *beforeTargetVal == *targetVal) { //if previous col hasn't been combined and if same val
+                int curIndex = (i * numCols) + targetCol;
+                int prevIndex = (i * numCols) + targetCol - 1;
+                if ((curIndex / numCols) == (prevIndex / numCols)) { //if in the same row
+                    *beforeTargetVal *= 2; //set the first block to double its value
+                    (*cur_game).score += *beforeTargetVal; //update score
+                    *targetVal = -1; //make second block empty
+                    lastCombined = targetCol - 1; //update lastCombined
+                    boardFlag = true; //indicate something changed
+                }
+            }
+        }
+    }
+    if (!boardFlag) return 0; //return 0 if nothing changed, if not return 1
     return 1;
-};
+}
 
 int move_d(game * cur_game){ //slide to the right
     //YOUR CODE STARTS HERE
+    int i, j; //iterators - row and col
+    bool boardFlag = false; //boardFlag detects if something has been moved or combined
+    int targetCol = -1; //last available empty location after current col
+    int lastCombined = -1; //last combined col
+    bool emptyFound = false; //true when there's an empty cell after current col
+    int numCols = (*cur_game).cols; //i don't want to type this out every time
 
+    for (i = 0; i < (*cur_game).rows; i++) {
+        lastCombined = -1; //resest lastCombined each row
+        for (j = (*cur_game).cols - 1; j > -1; j--) {
+            int *valAtij = &(*cur_game).cells[(i * numCols) + j];
+            if (*valAtij == -1) continue; //skip iteration if col j at row i is empty
+            emptyFound = false; //reset emptyFound
+            for (targetCol = (*cur_game).cols - 1; targetCol > j; targetCol--) {
+                if ((*cur_game).cells[(i * numCols) + targetCol] == -1) {
+                    emptyFound = true; //if empty location found after current col, set emptyFound flag and break
+                    break;
+                }
+            }
+            int *targetVal = &(*cur_game).cells[(i * numCols) + targetCol]; //pointer because i'm lazy
+            if (emptyFound) { 
+                *targetVal = *valAtij; //switch values if there's an empty spot to switch with
+                *valAtij = -1;
+                boardFlag = true; //indicate something changed
+            }
+            int *afterTargetVal = &(*cur_game).cells[(i * numCols) + targetCol + 1]; //pointer 2 because i'm lazy
+            if ((targetCol + 1) != lastCombined && *afterTargetVal == *targetVal) { //if previous col hasn't been combined and if same val
+                int curIndex = (i * numCols) + targetCol;
+                int nextIndex = (i * numCols) + targetCol + 1;
+                if ((curIndex / numCols) == (nextIndex / numCols)) { //if in the same row
+                    *afterTargetVal *= 2; //set the first block to double its value
+                    (*cur_game).score += *afterTargetVal; //update score
+                    *targetVal = -1; //make second block empty
+                    lastCombined = targetCol + 1; //update lastCombined
+                    boardFlag = true; //indicate something changed
+                }
+            }
+        }
+    }
+    if (!boardFlag) return 0; //return 0 if nothing changed, if not return 1
     return 1;
-};
+}
 
 int legal_move_check(game * cur_game)
 /*! Given the current game check if there are any legal moves on the board. There are
@@ -98,8 +288,28 @@ int legal_move_check(game * cur_game)
  */
 {
     //YOUR CODE STARTS HERE
-
-    return 1;
+    int i, j; //declare iterators
+    int numCols = (*cur_game).cols; //variables for rows and cols
+    int numRows = (*cur_game).rows;
+    for (i = 0; i < numRows; i++) {
+        for (j = 0; j < numCols; j++) {
+            int *valAtij = &(*cur_game).cells[(i * numCols) + j]; //value at [i][j] pointer
+            if (*valAtij == -1) return 1; //if empty spaces on board, return 1
+            if ((i + 1) < numRows && *valAtij == (*cur_game).cells[((i + 1) * numCols) + j]) return 1; //move_s check
+            if ((i - 1) > -1 && *valAtij == (*cur_game).cells[((i -1) * numCols) + j]) return 1; //move_w check
+            if ((j + 1) < numCols && *valAtij == (*cur_game).cells[(i * numCols) + j + 1]) { //move_d check
+                int curIndex = (i * numCols) + j;
+                int nextIndex = (i * numCols) + j + 1;
+                if ((curIndex / numCols) == (nextIndex / numCols)) return 1;
+            }
+            if ((j - 1) > -1 && *valAtij == (*cur_game).cells[(i * numCols) + j + 1]) { //move_a check
+                int curIndex = (i * numCols) + j;
+                int nextIndex = (i * numCols) + j + 1;
+                if ((curIndex / numCols) == (nextIndex / numCols)) return 1;
+            }
+        }
+    }
+    return 0; //if none of those work, there are no legal moves
 }
 
 
