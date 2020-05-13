9fe6550b32ef543451561a398fe7a0e0e30fddb4
Gaurav Raj <graj2@illinois.edu>
2020-05-13 08:39:28 -0500

    P3 Done

--- Regular_Final/dist/P3/binarytree.c
+++ Regular_Final/dist/P3/binarytree.c
@@ -51,5 +51,32 @@ NODE* create_tree(int a[], int size )
 // Return 0: if they are at different levels
 int same_level(NODE* root, int node1, int node2) {
   // Write your code here!
+  int level1, level2;
+  NODE* rootcopy = root;
+
+  if (root -> d == node1) level1 = 0;
+  if (root -> d == node2) level2 = 0;
+  while (rootcopy -> d != node1) {
+    if (node1 > rootcopy -> d) { 
+      rootcopy = rootcopy -> right;
+      level1++;
+    } else {
+      rootcopy = rootcopy -> left;
+      level1++;
+    }
+  }
+
+  while (root -> d != node2) {
+    if (node2 > root -> d) { 
+      root = root -> right;
+      level2++;
+    } else {
+      root = root -> left;
+      level2++;
+    }
+  }
+
+  if (level1 == level2) return 1;
+  return 0;
 } 
 
