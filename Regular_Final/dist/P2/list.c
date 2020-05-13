1943cbff4d71699cac982afccefc7e323c23bc93
Gaurav Raj <graj2@illinois.edu>
2020-05-13 09:52:10 -0500

    Finished P2

--- Regular_Final/dist/P2/list.c
+++ Regular_Final/dist/P2/list.c
@@ -7,8 +7,13 @@
 
 int changeDate(node* head, int custID, int newDate){
 //YOUR CODE STARTS HERE
-
-
+    while (head != NULL) {
+        if (head -> cust -> customerID == custID) {
+            head -> cust -> orderDate = newDate;
+            return 1;
+        }
+        head = head -> next;
+    }
     return 0;
 }
 
@@ -17,7 +22,12 @@ int changeDate(node* head, int custID, int newDate){
 
 void destroyList(node* head){
 //YOUR CODE STARTS HERE
-
-
-
+    node* headcopy = head;
+    while (head != NULL)
+    {   
+        headcopy = head;
+        free (head -> cust);
+        head = head -> next;
+        free (headcopy);
+    }
 }
